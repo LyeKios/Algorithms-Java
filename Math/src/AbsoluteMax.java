4254adb0984687fb459be40452e15ae7ac1fd15d
LyeKios <2546216709@qq.com>
2020-09-23 15:04:38 +0800

    math package

---
+++ Math/src/AbsoluteMax.java
@@ -0,0 +1,26 @@
+/*
+* Caculate the Max abs
+* eg: absMax([0, 5, 1, 11]) = 11, absMax([3 , -10, -2]) = -10
+ */
+
+import java.util.Arrays;
+
+public class AbsoluteMax {
+    public static void main(String[] args) {
+        int[] testArr = {1, 4, 16, -20}; //wanna got getAbsMax(testArr) = 20
+//        if (getAbsMax(testArr) != 2) throw new AssertionError();
+        assert getAbsMax(testArr) == -20;
+
+        System.out.println("The AbsMax of (" + Arrays.toString(testArr) + ") is "+ getAbsMax(testArr));
+
+    }
+
+    public static int getAbsMax(int[] Arr) {
+        int maxNum = Arr[0];
+        for (int j : Arr) {
+            maxNum = Math.abs(j) > Math.abs(maxNum) ? j : maxNum;
+        }
+        return maxNum;
+    }
+
+}
